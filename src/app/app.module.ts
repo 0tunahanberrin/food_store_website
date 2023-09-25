d08fc66009c054fe13479ffce8606297c4a783a4
0tunahanberrin <94645801+0tunahanberrin@users.noreply.github.com>
2023-09-25 22:37:34 +0300

    commit
    Next step is adding a map to the checkout page and creating a payment page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 //import { NgStarRatingModule } from 'ng-starrating';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,10 @@ import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { ReactiveFormsModule} from '@angular/forms';
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
+import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
+import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
 //import { RatingComponent } from 'ng-starrating/lib/rating.component';
 //import { RatingModule } from 'ng-starrating';
 
@@ -29,7 +33,12 @@ import { ReactiveFormsModule} from '@angular/forms';
     CartPageComponent,
     TitleComponent,
     NotFoundComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    CheckoutPageComponent,
+    RegisterPageComponent,
+    LoadingComponent,
+    CheckoutPageComponent,
+    OrderItemsListComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +47,9 @@ import { ReactiveFormsModule} from '@angular/forms';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
